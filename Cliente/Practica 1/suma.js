53772ffd4f804f645317e7d12b9174a7b4825f66
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-09-22 21:00:05 +0200

    Finished
    First assignment.

---
+++ Cliente/Practica 1/suma.js
@@ -0,0 +1,23 @@
+var n1 = 3;
+var n2 = 4;
+
+function suma(n1, n2){
+    return n1 + n2;
+}
+
+function resta(n1, n2){
+    return n1 - n2;
+}
+
+function multiplica(n1, n2){
+    return n1 * n2;
+}
+
+function divide(n1, n2){
+    return n1 / n2;
+}
+
+alert("Esta página dice\n El resultado de sumar 3 + 4 es" + " " + suma(n1,n2));
+alert("Esta página dice\n El resultado de sumar 3 + 4 es" + " " + resta(n1,n2));
+alert("Esta página dice\n El resultado de sumar 3 + 4 es" + " " + multiplica(n1,n2));
+alert("Esta página dice\n El resultado de sumar 3 + 4 es" + " " + divide(n1,n2));
