96629784d1ce58e8cf147a8895cd668e6ab72536
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-09-27 21:45:31 +0200

    dwes

---
+++ Servidor/main.js
@@ -0,0 +1,25 @@
+var mas = "";
+const DAW = document.getElementById('').textContent;
+const DWES = document.getElementById('').textContent;
+const DIW = document.getElementById('').textContent;
+const textoOriginal = document.getElementById('').textContent;
+
+
+function addSuma(){
+    document.getElementById("asignatura").value = "+";
+}
+
+// Función para agregar el símbolo "+" al texto
+function agregarSimbolo() {
+    // Obtener el texto actual
+    let textoActual = textoOriginal.textContent;
+
+    // Agregar el símbolo "+" al texto
+    textoActual += '+';
+
+    // Actualizar el texto en el elemento
+    textoOriginal.textContent = textoActual;
+}
+
+// Agregar un evento de clic al botón
+agregarButton.addEventListener('click', agregarSimbolo);
