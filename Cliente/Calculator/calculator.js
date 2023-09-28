d30db827e88833c52b40718185d7879dd0afeae3
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-09-28 18:44:50 +0200

    .

--- Cliente/Calculator/calculator.js
+++ Cliente/Calculator/calculator.js
@@ -1,4 +1,8 @@
 function appendToDisplay(value) {
+    var num1 = value;
+    if(document.getElementById('display').value==`(Math.sqrt())`){
+        let num = value;
+    }
     document.getElementById('display').value += value;
 }
 
@@ -12,5 +16,7 @@ function calculateResult() {
         document.getElementById('display').value = result;
     } catch (error) {
         document.getElementById('display').value = 'Error';
+
     }
 }
+
