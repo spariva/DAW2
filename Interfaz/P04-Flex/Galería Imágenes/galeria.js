6b3593245c3b981881aa978a7841250c0d4dd78a
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-10-18 19:46:33 +0200

    .
    cover fit

--- Interfaz/P04-Flex/Galería Imágenes/galeria.js
+++ Interfaz/P04-Flex/Galería Imágenes/galeria.js
@@ -1,6 +1,6 @@
 var img = ["https://makspariva.files.wordpress.com/2018/10/fireflies.jpg", "https://makspariva.files.wordpress.com/2018/09/lostinsociety.jpg", "https://makspariva.files.wordpress.com/2018/10/foundsilence.jpg", "https://makspariva.files.wordpress.com/2018/09/img_9724.jpg"];
-var img2 =["https://makspariva.files.wordpress.com/2018/07/img_0417.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_7000.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0622.jpg", "https://makspariva.files.wordpress.com/2018/09/los-abrigos-son-para-los-lunes-de-lluvia3.jpg"]
-var img3 =["https://makspariva.files.wordpress.com/2018/10/scan10098.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9626.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9625.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0516.jpg"]
+var img2 =["https://makspariva.files.wordpress.com/2018/07/img_0417.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_7000.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9625.jpg", "https://makspariva.files.wordpress.com/2018/09/los-abrigos-son-para-los-lunes-de-lluvia3.jpg"]
+var img3 =["https://makspariva.files.wordpress.com/2018/10/scan10098.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9626.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0622.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0516.jpg"]
 const imagenElemento = document.getElementById('miImagen');
 const imagenElemento2 = document.getElementById('miImagen2');
 const imagenElemento3 = document.getElementById('miImagen3');
