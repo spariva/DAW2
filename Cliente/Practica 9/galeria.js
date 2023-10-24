67916c19143b5ca2c08f93bac428119cefec6c59
Maki <makspariva@gmail.com>
2023-10-25 00:02:19 +0200

    =) IT WORKS =)

--- Cliente/Practica 9/galeria.js
+++ Cliente/Practica 9/galeria.js
@@ -1,20 +1,28 @@
-var img = ["https://makspariva.files.wordpress.com/2018/10/fireflies.jpg", "https://makspariva.files.wordpress.com/2018/09/lostinsociety.jpg", "https://makspariva.files.wordpress.com/2018/10/foundsilence.jpg", "https://makspariva.files.wordpress.com/2018/09/img_9724.jpg"];
-var img2 =["https://makspariva.files.wordpress.com/2018/07/img_0417.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_7000.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9625.jpg", "https://makspariva.files.wordpress.com/2018/09/los-abrigos-son-para-los-lunes-de-lluvia3.jpg"]
-var img3 =["https://makspariva.files.wordpress.com/2018/10/scan10098.jpg", "https://makspariva.files.wordpress.com/2018/10/img_9626.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0622.jpg", "https://makspariva.files.wordpress.com/2018/10/mg_0516.jpg"]
-const imagenElemento = document.getElementById('primero');
-const imagenElemento2 = document.getElementById('miImagen2');
-const imagenElemento3 = document.getElementById('miImagen3');
-var contador = 0;
-console.log(imagenElemento.src); 
-function cambiarImagen() {  
-    if(contador>=img.length){
-        imagenElemento.src = img[0];
-        imagenElemento2.src = img2[0];
-        imagenElemento3.src = img3[0];
-        contador = 0;
+//lo ideal sería una sola función, vamos a ello.
+var listas =["https://open.spotify.com/embed/playlist/6T442Dd7j4FYpP7rtCh4Mp?utm_source=generator","https://open.spotify.com/embed/playlist/6hW4rvfy42xQb0I8A99ysJ?utm_source=generator","https://open.spotify.com/embed/playlist/6lHivMtxlldZdqEvpwGRxZ?utm_source=generator"];
+const carouselActive = document.getElementsByClassName('active');
+const carousel0 = document.getElementById('primero');
+const carousel1 = document.getElementById('segundo');
+const carousel2 = document.getElementById('tercero');
+const playlist = document.getElementById('playlista');
+
+console.log(playlist.src); 
+function cambiarImagen(indice) {
+    switch (indice) {
+        case 0:
+            carousel0.classList.add('active');
+            playlist.src = listas[0];
+            break;
+        case 1:
+            carousel1.classList.add('active');
+            playlist.src = listas[1];
+            break;
+        case 2:
+            carousel2.classList.add('active');  
+            playlist.src = listas[2];  
+            break;
+        default:
+            alert("Error");
+            break;
     }
-    imagenElemento.src = img[contador];
-    imagenElemento2.src = img2[contador];
-    imagenElemento3.src = img3[contador];
-    contador ++;
 }
