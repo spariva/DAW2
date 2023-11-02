fbc61740be9adbe2992f01a145804452c97d0847
Maki <makspariva@gmail.com>
2023-11-02 15:40:39 +0100

    Mouseover resaltado

--- Cliente/Practica 12 ListenersTinder/faildate.js
+++ Cliente/Practica 12 ListenersTinder/faildate.js
@@ -1,6 +1,7 @@
 const endingCry = "https://i.pinimg.com/originals/e2/08/95/e208950e412f5c9b9a90a4fb50ef51cc.jpg";
 const endingGhost = "https://custom-progress-bar.com/cdn/images/95/pixel-ghost-custom-progress-bar-m.png";
 const endingFire = "https://preview.redd.it/this-is-fine-pixelart-versi%C3%B3n-artist-me-v0-bhpx9mpaowrb1.png?width=640&crop=smart&auto=webp&s=aa0847aaf28b8529b9f919bc0a9ebc9fcf08be59";
+const candidates = document.getElementById("candidates");
 const anthony = document.getElementById("Anthony");
 const ghostkid = document.getElementById("Ghostkid");
 const zoe = document.getElementById("Zoe");
@@ -8,18 +9,19 @@ const message = document.getElementById("message");
 const dating = document.getElementsByClassName("dating");
 const deleting = document.getElementsByClassName("deleting");
 
-dating.addEventListener("mouseover", function(e){
+candidates.addEventListener("mouseover", function(e){
+    if(e.target.classList.contains("dating")){
     e.target.classList.add("resaltado-pastel");
+    }
+    if(e.target.classList.contains("deleting")){
+        e.target.classList.add("resaltado-agro");
+    }
 });
-dating.addEventListener("mouseout", function(e){
+candidates.addEventListener("mouseout", function(e){
     e.target.classList.remove("resaltado-pastel");
-});
-deleting.addEventListener("mouseover", function(e){
-    e.target.classList.add("resaltado-agro");
-});
-deleting.addEventListener("mouseout", function(e){
     e.target.classList.remove("resaltado-agro");
 });
+
 
 
 
