326781ddd4feb5ee692db1151f13e6278673f10f
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-12-12 20:08:36 +0100

    Aviso cookies

--- Servidor/Tema 3/cookie 1/info.php
+++ Servidor/Tema 3/cookie 1/info.php
@@ -5,8 +5,27 @@ include('colors.php');
 $color = $colors[0];
 if (isset($_COOKIE['color']) && in_array($_COOKIE['color'], $colors)) {
     $color = $_COOKIE['color'];
-    setcookie('color', $_COOKIE('color'), time() + (60*60*24*7));//cada vez que revisites se vuelve a setear para que te vuelva a dar una semana.
+    setcookie('color', $_COOKIE['color'], time() + (60*60*24*7));//cada vez que revisites se vuelve a setear para que te vuelva a dar una semana.
 }
+
+function imprimirAvisoCookies() {
+    echo '<div style="background-color: #f8f8f8; padding: 10px; text-align: center; position: absolute">';
+    echo 'Esta web utiliza cookies. Acepta el uso de cookies o lo lamentarás.';
+    echo '<br>';
+    echo '<a href="?privacy=1">Aceptar</a>';
+    echo '</div>';
+}
+
+if (!isset($_COOKIE['privacy'])){
+    imprimirAvisoCookies();    
+
+    if (isset($_GET['privacy']) && $_GET['privacy'] == '1'){
+        setcookie('privacy', '1', time() + (60*60*24*7));
+    } else {
+        setcookie('privacy', '0', time() + (60*60*24*7));
+    }
+}
+
 
 ?>
 
@@ -52,6 +71,7 @@ if (isset($_COOKIE['color']) && in_array($_COOKIE['color'], $colors)) {
             <h2>Blog Post 3</h2>
             <p>Etiam auctor, nunc id aliquam luctus, nisl nunc fringilla justo, id lacinia nunc nunc in nunc.</p>
         </div>
+        <a href="pref.php">Change color</a>
     </div>
 </body>
 </html>
