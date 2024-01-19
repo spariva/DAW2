5178462fe8ba190bad0f13a17ae78bd983873ffa
Maki <makspariva@gmail.com>
2024-01-19 16:14:52 +0100

    animación terminada

--- Interfaz/Animaciones/Gigante Azul/jump.js
+++ Interfaz/Animaciones/Gigante Azul/jump.js
@@ -2,19 +2,18 @@
 //Consts:
 const gigant = document.getElementById("gigant");
 
+
+function jump(k) {
+    if (k.keyCode === 32) {
+        document.getElementById("gigant").classList.toggle("jump");
+    }
+}
+
 //Event listener:
 document.addEventListener("keypress", jump);
 
-function jump() {
-    if (keypress.keyCode === 32) {
-        document.getElementById("gigant").classList.add("jump");
-        setTimeout(function () {
-            document.getElementById("gigant").classList.remove("jump");
-        }, 500);
-    }
-}
 
 document.addEventListener("beforeunload", (event) => {
     event.preventDefault();
     alert("No te vayas :( que el gigante casi ha llegado a su destino");
-  });
+});
