d1164b77b819911eb6650160a9541e3d43f1b2b2
Maki <makspariva@gmail.com>
2024-01-25 09:14:11 +0100

    npm

--- Servidor/Python/Pycheck/1-Control de Flujo/Bucles/prime.py
+++ Servidor/Python/Pycheck/1-Control de Flujo/Bucles/prime.py
@@ -15,16 +15,16 @@
 │ 8 │ 67        │ True          │
 └───┴───────────┴───────────────┘"""
 
+
 def run(points: str) -> str:
     # TU CÓDIGO AQUÍ
 
-    num = len(points) -1
+    num = len(points) - 1
     puntosA = 0
     puntosB = 0
 
-    while (num >= 0):
-
-        if (points[num] == 'A'):
+    while num >= 0:
+        if points[num] == "A":
             puntosA += 1
         else:
             puntosB += 1
@@ -32,9 +32,10 @@ def run(points: str) -> str:
         num -= 1
 
     if puntosA > puntosB:
-        return 'A'
-    else: 
-        return 'B'
+        return "A"
+    else:
+        return "B"
 
-if __name__ == '__main__':
-    run('ABAABA')
+
+if __name__ == "__main__":
+    run("ABAABA")
