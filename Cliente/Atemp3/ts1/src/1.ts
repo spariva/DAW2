c8e1791475ee7333b0a61631d6e0a62ff4c14151
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2024-02-06 18:31:19 +0100

    Update 1.ts
    en teoría ahora funciona...

--- Cliente/Atemp3/ts1/src/1.ts
+++ Cliente/Atemp3/ts1/src/1.ts
@@ -1,22 +1,35 @@
-const boton: HTMLButtonElement = document.getElementById('boton') as HTMLButtonElement
-const inputName: HTMLInputElement = document.getElementById('inputName') as HTMLInputElement
-const inputAge: HTMLInputElement = document.getElementById('inputAge') as HTMLInputElement
-const divResult: HTMLDivElement = document.getElementById('result') as HTMLDivElement
+// app.ts
+document.addEventListener('DOMContentLoaded', () => {
+    const boton: HTMLButtonElement = document.getElementById('boton') as HTMLButtonElement
+    const inputName: HTMLInputElement = document.getElementById('inputName') as HTMLInputElement
+    const inputAge: HTMLInputElement = document.getElementById('inputAge') as HTMLInputElement
+    const divResult: HTMLDivElement = document.getElementById('result') as HTMLDivElement
+    const form = document.getElementById('basicForm') as HTMLFormElement;
 
-function saludar(nombre: string, edad: number): string {
-    return "¡Hola, " + nombre + " tienes " + edad + " años!"
-}
+    function saludar(nombre: string, edad: number): string {
+        return "¡Hola, " + nombre + " tienes " + edad + " años!"
+    }
 
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Prevents the default form submission
+    });
 
-boton.addEventListener('click', (event) => {
-    event.preventDefault();
-    // const valorInputName: string = inputName.value
+    boton.addEventListener('click', ()=> {
+        const valorInputName: string = inputName.value
+        const valorInputAge: number = parseInt(inputAge.value)
+        divResult.innerHTML = saludar(valorInputName, valorInputAge)
 
-    // const valorInputAge: number = parseInt(inputAge.value)
+    });
 
-    alert(saludar(inputName.value, parseInt(inputAge.value)))
-    divResult.innerHTML = saludar(inputName.value, parseInt(inputAge.value))
 
 });
+
+
+
+
+
+
+
+
 
 
