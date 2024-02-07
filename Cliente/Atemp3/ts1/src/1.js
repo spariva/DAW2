314f8d09d15d8a2456406aa6be508637fbf200e4
Maki <makspariva@gmail.com>
2024-02-07 15:28:08 +0100

    ts input id changed

--- Cliente/Atemp3/ts1/src/1.js
+++ Cliente/Atemp3/ts1/src/1.js
@@ -1,14 +1,18 @@
-var boton = document.getElementById('boton');
-var inputName = document.getElementById('inputName');
-var inputAge = document.getElementById('inputAge');
-var divResult = document.getElementById('result');
-function saludar(nombre, edad) {
-    return "¡Hola, " + nombre + " tienes " + edad + " años!";
-}
-boton.addEventListener('click', function (event) {
-    event.preventDefault();
-    // const valorInputName: string = inputName.value
-    // const valorInputAge: number = parseInt(inputAge.value)
-    alert(saludar(inputName.value, parseInt(inputAge.value)));
-    divResult.innerHTML = saludar(inputName.value, parseInt(inputAge.value));
+document.addEventListener('DOMContentLoaded', function () {
+    var boton = document.getElementById('boton');
+    var inputName = document.getElementById('inputName');
+    var inputAge = document.getElementById('inputAge');
+    var divResult = document.getElementById('result');
+    var form = document.getElementById('basicForm');
+    function saludar(nombre, edad) {
+        return "¡Hola, " + nombre + " tienes " + edad + " años!";
+    }
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevents the default form submission        
+    });
+    boton.addEventListener('click', function () {
+        var valorInputName = inputName.value;
+        var valorInputAge = parseInt(inputAge.value);
+        divResult.innerHTML = saludar(valorInputName, valorInputAge);
+    });
 });
--- Cliente/Atemp3/ts1/src/1.ts
+++ Cliente/Atemp3/ts1/src/1.ts
@@ -1,20 +1,20 @@
-// app.ts
 document.addEventListener('DOMContentLoaded', () => {
     const boton: HTMLButtonElement = document.getElementById('boton') as HTMLButtonElement
     const inputName: HTMLInputElement = document.getElementById('inputName') as HTMLInputElement
     const inputAge: HTMLInputElement = document.getElementById('inputAge') as HTMLInputElement
     const divResult: HTMLDivElement = document.getElementById('result') as HTMLDivElement
-    const form = document.getElementById('basicForm') as HTMLFormElement;
+    const form: HTMLFormElement = document.getElementById('basicForm') as HTMLFormElement
 
     function saludar(nombre: string, edad: number): string {
         return "¡Hola, " + nombre + " tienes " + edad + " años!"
     }
 
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevents the default form submission
+        event.preventDefault() // Prevents the default form submission        
+
     });
 
-    boton.addEventListener('click', ()=> {
+    boton.addEventListener('click', () => {
         const valorInputName: string = inputName.value
         const valorInputAge: number = parseInt(inputAge.value)
         divResult.innerHTML = saludar(valorInputName, valorInputAge)
