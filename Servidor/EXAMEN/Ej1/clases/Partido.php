bf530353c32d5b7facda3f87d248f77caca335b8
Maki <makspariva@gmail.com>
2024-03-01 06:22:58 +0100

    ...

--- Servidor/EXAMEN/Ej1/clases/Partido.php
+++ Servidor/EXAMEN/Ej1/clases/Partido.php
@@ -1,11 +1,11 @@
 <?php 
 class Partido {
-    public $persona1;
-    public $persona2;
+    public Persona $persona1;
+    public Persona $persona2;
     public $resultado;
     public $horaDeJuego;
 
-    public function __construct(Persona $persona1, $persona2, string $resultado, $horaDeJuego) {
+    public function __construct(Persona $persona1, Persona $persona2, string $resultado, string $horaDeJuego) {
         $this->persona1 = $persona1;
         $this->persona2 = $persona2;
         $this->resultado = $resultado;
--- Servidor/EXAMEN/Ej1/funciones.php
+++ Servidor/EXAMEN/Ej1/funciones.php
@@ -10,17 +10,6 @@ spl_autoload_register(
     }
 );
 
-
-// spl_autoload_register(function($class) {
-//     $file = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-//     $path = DOC_ROOT . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . $file . ".php";
-//     if (file_exists($path)) {
-//         require $path;
-//     } else {
-//         throw new Exception("Cannot load class: $class");
-//     }
-// });
-
 function randomPeople(int $numberPeople) : array {
     $people = [];
     $nombres = explode("\n" , file_get_contents(NOMBRES));
