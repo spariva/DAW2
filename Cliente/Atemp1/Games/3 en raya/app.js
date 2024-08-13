32a45c7bee7d978a132613cd8b0320eaaf84f0b3
Maki <makspariva@gmail.com>
2024-08-13 03:00:21 +0200

    Array iteration

--- Cliente/Atemp1/Games/3 en raya/app.js
+++ Cliente/Atemp1/Games/3 en raya/app.js
@@ -12,7 +12,7 @@ let gameActive = true;
 // }
 
 function printBoard() {
-    console.log(`
+  console.log(`
       ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
       ---------
       ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
@@ -21,58 +21,62 @@ function printBoard() {
     `);
 }
 
-function handleMove(position){
-    if(gameBoard[position] === ' '){
-        gameBoard[position] = currentPlayer;
-    } else {
-        console.log('You can´t go there!');
-        return false;
-    }
+function handleMove(position) {
+  if (gameBoard[position] === ' ') {
+    gameBoard[position] = currentPlayer;
+  } else {
+    console.log('You can´t go there!');
+    return false;
+  }
 
-    if (checkWin()) {
-        printBoard();
-        console.log(`Player ${currentPlayer} wins!`);
-        gameActive = false;
-        return true;
-      }
+  if (checkWin()) {
+    printBoard();
+    console.log(`Player ${currentPlayer} wins!`);
+    gameActive = false;
+    return true;
+  }
 
-    if (gameBoard.includes(' ')) {
-        printBoard();
-        console.log('The game is a tie!');
-        gameActive = false;
-        return true;
-      }
+  if (gameBoard.includes(' ')) {
+    printBoard();
+    console.log('The game is a tie!');
+    gameActive = false;
+    return true;
+  }
 
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
 
 function checkWin() {
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-  
-    for (let condition of winConditions) {
-      let a = condition[0];
-      let b = condition[1];
-      let c = condition[2];
-  
-      if (gameBoard[a] === ' ' || gameBoard[b] === ' ' || gameBoard[c] === ' ') {
-        continue;
-      }
-  
-      if (gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
-        return true;
-      }
-    }
-  
-    return false;
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // for (let condition of winConditions) {
+  //   let a = condition[0];
+  //   let b = condition[1];
+  //   let c = condition[2];
+
+  //   if (gameBoard[a] === ' ' || gameBoard[b] === ' ' || gameBoard[c] === ' ') {
+  //     continue;
+  //   }
+
+  //   if (gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
+  //     return true;
+  //   }
+  // }
+
+  //return false si no encuentra ninguna condición de victoria y true en cuanto encuentre una.
+  return winConditions.some(condition => {
+    const [a, b, c] = condition;
+    return gameBoard[a] === currentPlayer && gameBoard[b] === currentPlayer && gameBoard[c] === currentPlayer;
+  });
 }
 
 
